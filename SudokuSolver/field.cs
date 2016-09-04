78c2d7071a0db0a0589ba1bece65576e3925dda8
WinApp ExamRef70484 <examref70484@outlook.com>
2016-09-04 22:20:10 +0200

    Added the most basic of items in the sudoku puzzle; The field

---
+++ SudokuSolver/field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver {
+    class Field {
+        public List<int> PossibleNumbers { get; } = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public int RowNumber { get; }
+        public int ColumnNumber { get; }
+        public int CellNumber { get { return RowNumber* ColumnNumber +RowNumber; } }
+    }
+}

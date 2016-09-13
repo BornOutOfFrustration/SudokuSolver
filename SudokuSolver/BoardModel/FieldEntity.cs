dbac017060597efe12404b5300288b0686579358
WinApp ExamRef70484 <examref70484@outlook.com>
2016-09-13 23:21:58 +0200

    Introduced the name 'Entity' in the business objects

--- SudokuSolver/BoardModel/FieldEntity.cs
+++ SudokuSolver/BoardModel/FieldEntity.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SudokuSolver {
-    class Field {
+    class FieldEntity {
         public List<int> PossibleNumbers { get; } = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         public int RowNumber { get; }
         public int ColumnNumber { get; }

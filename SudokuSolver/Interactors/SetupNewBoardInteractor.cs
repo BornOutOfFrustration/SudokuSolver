10ad7f21148dd8fe89d20bd11c430687bc3c1d1d
WinApp ExamRef70484 <examref70484@outlook.com>
2016-09-14 00:29:25 +0200

    Set up grid with rows and columns

--- SudokuSolver/Interactors/SetupNewBoardInteractor.cs
+++ SudokuSolver/Interactors/SetupNewBoardInteractor.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SudokuSolver {
-    class SudokuSolverViewModel {
-        public Dictionary<int[,], List<int>> Cells { get; }
-        
+namespace SudokuSolver.Interactors {
+    class SetupNewBoardInteractor {
     }
 }
--- SudokuSolver/SudokuSolverModel.cs
+++ SudokuSolver/SudokuSolverModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SudokuSolver.BoardModel;
 
 namespace SudokuSolver
 {
     class SudokuSolverModel
     {
+        BoardEntity Board { get; } = new BoardEntity();
     }
 }

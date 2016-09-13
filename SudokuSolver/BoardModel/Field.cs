037f8b4d0ab3fd2920a90c44937eebc8a1ab0139
WinApp ExamRef70484 <examref70484@outlook.com>
2016-09-13 23:07:41 +0200

    Changed the folder structure

--- SudokuSolver/BoardModel/Field.cs
+++ SudokuSolver/BoardModel/Field.cs
@@ -9,6 +9,5 @@ namespace SudokuSolver {
         public List<int> PossibleNumbers { get; } = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         public int RowNumber { get; }
         public int ColumnNumber { get; }
-        public int CellNumber { get { return RowNumber* ColumnNumber +RowNumber; } }
     }
 }

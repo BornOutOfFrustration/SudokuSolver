031807155392adc29da91f35eddb50b71c6e22c5
WinApp ExamRef70484 <examref70484@outlook.com>
2017-01-02 21:00:54 +0100

    Made test for Board.ColumnEntity work

--- SudokuSolver/SudokuBoard/BoardEntity.cs
+++ SudokuSolver/SudokuBoard/BoardEntity.cs
@@ -8,17 +8,26 @@ using System.Collections.ObjectModel;
 namespace SudokuSolver.SudokuBoard {
     public class BoardEntity {
         public BoardEntity() {
-            List<RowEntity> list = new List<RowEntity>();          
+            List<RowEntity> list1 = new List<RowEntity>();          
             
             for (int i = 0; i < 9; i++) {
-                list.Add(new RowEntity());
+                list1.Add(new RowEntity());
             }
 
-            Rows = new ReadOnlyCollection<RowEntity>(list);
+            Rows = new ReadOnlyCollection<RowEntity>(list1);
+
+            List<ColumnEntity> list2 = new List<ColumnEntity>();
+
+            for (int i = 0; i < 9; i++) {
+                list2.Add(new ColumnEntity());
+            }
+
+            Columns = new ReadOnlyCollection<ColumnEntity>(list2);
         }
 
 
         public ReadOnlyCollection<RowEntity> Rows { get; }
+        public ReadOnlyCollection<ColumnEntity> Columns { get; }
         //        public ReadOnlyCollection<NineSetEntity> Columns { get; } 
         //        public ReadOnlyCollection<NineSetEntity> Rows { get; }
         //        public ReadOnlyCollection<NineSetEntity> Squares { get; } = new ReadOnlyCollection<NineSetEntity>(
@@ -30,5 +39,7 @@ namespace SudokuSolver.SudokuBoard {
 
     public class RowEntity {
 
+    }
+    public class ColumnEntity {
     }
 }
